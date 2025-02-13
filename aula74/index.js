2912af34af0f0b344aaab01f8bb0b738eb8970d7
Alessandro Ramiro <ramsxd67@gmail.com>
2025-02-13 10:17:48 -0300

    Prototypes

---
+++ aula74/index.js
@@ -0,0 +1,17 @@
+// Construtora -> molde (classe)
+function Pessoa(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+// Pessoa.prototype = pessoa1.__proto__
+
+Pessoa.prototype.nomeCompleto = () => this.nome + " " + this.sobrenome;
+
+// instância
+const pessoa1 = new Pessoa("Luiz", "O."); // <- Pessoa = Função construtora
+const pessoa2 = new Pessoa("Maria", "A."); // <- Pessoa = Função construtora
+const data = new Date() // <-- Date = Função construtora
+
+console.dir(pessoa1);
+console.dir(data);
