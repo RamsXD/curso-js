9335d25d441fad88b6a441e61f132055be383281
Alessandro Ramiro <ramsxd67@gmail.com>
2025-02-18 20:31:09 -0300

    promises

---
+++ aula86/index.js
@@ -0,0 +1,33 @@
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    if (typeof msg !== "string") reject("BAD VALUE");
+    setTimeout(() => {
+      resolve(msg);
+    }, tempo);
+  });
+}
+
+esperaAi("Conexão com o BD", rand(1, 3))
+  .then((resposta) => {
+    console.log(resposta);
+    return esperaAi("Buscando dados na BASE", rand(1, 3));
+  })
+  .then((resposta) => {
+    console.log(resposta);
+    return esperaAi("Tratando so dados da BASE", rand(1, 3));
+  })
+  .then((resposta) => {
+    console.log(resposta);
+  })
+  .then(() => {
+    console.log("Exibe dados na tela");
+  })
+  .catch((e) => {
+    console.log("ERRO:", e);
+  });
