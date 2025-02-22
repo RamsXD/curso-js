74ffcfe06d133732abfa6f85e5eed1ec803b27d7
Alessandro Ramiro <ramsxd67@gmail.com>
2025-02-22 10:38:27 -0300

    ES6 Modules - Import/ Export

---
+++ aula94/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("rams","Luiz","Miranda",30),console.log(10)})();
+//# sourceMappingURL=bundle.js.map
---
+++ aula94/src/index.js
@@ -0,0 +1,6 @@
+import { idade, nome as nome2, sobrenome, soma } from "./modulo1";
+
+const nome = "rams";
+
+console.log(nome, nome2, sobrenome, idade);
+console.log(soma(5, 5));
